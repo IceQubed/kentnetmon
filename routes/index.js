eaf21bd2b27cf539ad1e25a7ddf2f90925293613
Ben Adamson <Ben Adamson>
2016-10-28 00:01:23 +0100

    Fixed titles

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,21 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'Dashboard'
+        title: 'NetMon Dashboard'
     });
 });
 
 /* GET agents page. */
 router.get('/agents', function (req, res, next) {
     res.render('agents', {
-        title: 'Agents'
+        title: 'NetMon Agents'
     });
 });
 
 /* GET settings page. */
 router.get('/settings', function (req, res, next) {
     res.render('settings', {
-        title: 'Settings'
+        title: 'NetMon Settings'
     });
 });
 
