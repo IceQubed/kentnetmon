24360d5571a2f84d71477c29e4f7b975c8a8623e
Ben Adamson <Ben Adamson>
2016-11-11 14:54:05 +0000

    move form

--- routes/agents.js
+++ routes/agents.js
@@ -3,14 +3,6 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Comment = mongoose.model('comments');
 
-///* GET agents page. */
-//router.get('/agents', function (req, res, next) {
-//    res.render('agents', {
-//        title: 'KentNetMon Agents'
-//    });
-//});
-
-
 /* GET form. */
 router.get('/', function (req, res) {
     Comment.find(function (err, comments) {
