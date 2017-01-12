dfa465bdd7fe2dd6d3c34768943049fa21cfd23d
Ben Adamson <Ben Adamson>
2017-01-12 20:22:24 +0000

    fixed agent testing, now agent details are added to tests!

--- controllers/iperf.js
+++ controllers/iperf.js
@@ -5,27 +5,32 @@ var mongoose = require('mongoose');
 var Result = mongoose.model('results');
 var newResult = new Result();
 var Comment = mongoose.model('comments');
+var Agent = mongoose.model('agents');
 
-router.get('/:ip', function (req, res) {
-    child_process.execFile('iperf3', ['-c', req.params.ip, '-J'], function (error, stdout, stderr) { //execute iperf3 with arguments, takes ip as parameter, UNSAFE.
+router.get('/:agentid', function (req, res) {
+    Agent.findById(req.params.agentid, function (err, agent) {
+        child_process.execFile('iperf3', ['-c', agent.ipAddr, '-J'], function (error, stdout, stderr) { //execute iperf3 with arguments, takes ip as parameter
 
-        console.log(stdout);
-        newResult = JSON.parse(stdout); //parse text output to JSON format
-        console.log('\n \n \n \n \n \n');
-        console.log(newResult); //log results
+            console.log(stdout);
+            newResult = JSON.parse(stdout); //parse text output to JSON format
+            console.log('\n \n \n \n \n \n');
+            console.log(newResult); //log results
 
-        Result(newResult).save(function (err) {
-            if (err) {
-                res.status(500).json({
-                    error: err.message
+            newResult.agent = agent;
+
+            Result(newResult).save(function (err) {
+                if (err) {
+                    res.status(500).json({
+                        error: err.message
+                    });
+                    throw err;
+                }
+
+                res.status(200).json({
+                    ok: true
                 });
-                throw err;
-            }
-
-            res.status(200).json({
-                ok: true
+                console.log('Item added!'); //when added log in console
             });
-            console.log('Item added!'); //when added log in console
         });
     });
 });
